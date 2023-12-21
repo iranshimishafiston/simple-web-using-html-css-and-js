af44741c1a94da647f8091441ba798e84ece5b5d
iranshimishafiston <131974757+iranshimishafiston@users.noreply.github.com>
2023-12-21 07:37:10 +0200

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,28 @@
+function fk_bio() {
+    document.getElementById('body').style.display='block';
+}
+
+function gisha(){
+    document.getElementById('gisha').style.display='block';
+}
+
+function nova(){
+    document.getElementById('icee').style.display='block';
+}
+
+function lost(){
+    document.getElementById('lostooh').style.display='block';
+}
+
+function phillipe(){
+    document.getElementById('phillipe').style.display='block';
+}
+
+function gafezi(){
+    document.getElementById('gafezi').style.display='block';
+}
+
+function navigation(){
+    document.getElementById('navigation').style.display='block';
+    document.getElementById('home').style.display='none';
+}
